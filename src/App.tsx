60058937abaf53148715437595ae1aa0e16d8a2a
dz_kazhtay <dkazhtai21@mail.ru>
2022-11-07 18:28:55 +0600

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+  const state = useSelector(state => state)
+
+  useEffect(() => {
+    console.log(123, state)
+  } ,[])
   return (
     <div className="App">
-123
+        <h1>123</h1>
     </div>
   );
 }
